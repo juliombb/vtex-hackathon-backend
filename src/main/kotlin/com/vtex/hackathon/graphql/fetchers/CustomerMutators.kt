14fdfcd2c3b7c57ee8052c5703abc332707c95b0
Alexandre Ladeira <alexandreladeira13@gmail.com>
2019-05-31 05:02:28 -0300

    market changes + CustomerMutators

--- src/main/kotlin/com/vtex/hackathon/graphql/fetchers/CustomerMutators.kt
+++ src/main/kotlin/com/vtex/hackathon/graphql/fetchers/CustomerMutators.kt
@@ -1,10 +1,64 @@
 package com.vtex.hackathon.graphql.fetchers
 
+import com.vtex.hackathon.graphql.model.Customer
+import com.vtex.hackathon.graphql.model.Gender
+import graphql.schema.DataFetcher
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import org.springframework.stereotype.Component
 
 /**
  * @author Júlio Moreira Blás de Barros (julio.barros@movile.com)
  * @since 5/31/19
  */
-class CustomerMutators(private val jdbc: NamedParameterJdbcTemplate) {
+@Component
+class CustomerMutators(
+    private val jdbc: NamedParameterJdbcTemplate,
+    private val wishListBaseFetcher: DataFetcher<Customer>,
+    private val clock: Clock
+) {
+    val creator: DataFetcher<Customer> =
+        DataFetcher { env ->
+            val customerId = env.getArgument<String>("id").toLong()
+            val email = env.getArgument<String>("email")
+            val name = env.getArgument<String>("name")
+            val cpf = env.getArgument<String>("cpf")
+            val gender = env.getArgument<String>("gender")
+            val phone = env.getArgument<String>("phone")
+            val birthDate = env.getArgument<String>("phone")
+
+            val id = jdbc.query(
+                INSERT_CUSTOMER_QUERY,
+                mapOf(
+                    CUSTOMER_ID to customerId,
+                    NAME to name,
+                    CPF to cpf,
+                    EMAIL to email,
+                    PHONE to phone,
+                    GENDER to gender,
+                    BIRTH_DATE to birthDate)
+            ) { rs, _ -> rs.getLong(1) }.first()
+
+            Customer(
+                id = customerId,
+                name = name,
+                cpf = cpf,
+                email = email,
+                phone = phone,
+                gender = gender,
+                birthDate = birthDate
+                )
+        }
+
+    companion object {
+        const val CUSTOMER_TABLE = "customer"
+        const val CUSTOMER_ID = "id"
+        const val EMAIL = "email"
+        const val PHONE = "phone"
+        const val NAME = "name"
+        const val CPF = "cpf"
+        const val GENDER = "gender"
+        const val BIRTH_DATE = "birth_date"
+
+        val INSERT_CUSTOMER_QUERY = "INSERT INTO "
+    }
 }
--- src/main/kotlin/com/vtex/hackathon/graphql/model/Market.kt
+++ src/main/kotlin/com/vtex/hackathon/graphql/model/Market.kt
@@ -2,5 +2,7 @@ package com.vtex.hackathon.graphql.model
 
 data class Market(
     val id: Long? = null,
-    val address: String? = null
+    val address: String? = null,
+    val asset: String? = null,
+    val name: String? = null
 )
